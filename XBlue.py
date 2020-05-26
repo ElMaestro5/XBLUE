2d03a2a597b0424edf6c4b560b6f0ffc64128451
hugo <56563736+hug016@users.noreply.github.com>
2020-05-26 15:41:32 +0200

    Update XBlue.py

--- XBlue.py
+++ XBlue.py
@@ -30,13 +30,14 @@ def mainT(url):
         """
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        r = opener.open('https://www.asos.com/')
+        r = opener.open('https://french.alibaba.com')
         urlT = url.split('//')
         page = r.read()
         p = page.decode("UTF-8")
         soup = BeautifulSoup(p,'html.parser')
         form = soup.find_all("form")
         t={}
+        p=''
         for e in form:
                 for o in tab:
                         if e.get("role"):
@@ -56,9 +57,10 @@ def mainT(url):
                                 #recuperer tout les input du form
                                 a = e.findAll("input")
                                 if urlT[1] in z:
-                                        req =  'https:/' +z 
+                                        req =  'https:/' +z + '?'
+                                        
                                 else:
-                                        req = url +  z 
+                                        req = url +  z + '?'
                                         
                                 for b in a:
                                         #recuperer le name= du input
@@ -66,10 +68,14 @@ def mainT(url):
                                                 if b.get('value'):
                                                         if b.get('value') == '':
                                                                 t[b.get("name")] = payload
+                                                                req =  req + b.get("name") + "=" + payload
+
                                                         else :
                                                                 t[b.get("name")] = b.get('value')
+                                                                req =  req + b.get("name") + "=" + b.get('value')+ "&"
                                                 else:
                                                         t[b.get("name")] = payload
+                                                        req =  req + b.get("name") + "=" + payload
 
 
 
@@ -78,32 +84,42 @@ def mainT(url):
 
 
         headers = {}
+        print("request send : " + req )
         headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-        re = urllib.request.Request(req, headers = headers)
-        resp = urllib.request.urlopen(re)
-        respData = resp.read()
-        a = respData.decode("UTF-8")
-        sou = BeautifulSoup(a,'html.parser')
-                        
-        if payload in str(respData):
-                print('the payload is reflected on this page ... the site is potentially XSS vulnerable')
-                rep = input('Do you want to enter another url ?(y or n) ')
-                if rep == "Y" or rep == "y":
-                        rl = input(colored.green('Please enter an another url ?(https://www.example.com) :'))
-                        mainT(rl)
+        try:
+                re = urllib.request.Request(req, headers = headers)
+                resp = urllib.request.urlopen(re)
+                respData = resp.read()
+                a = respData.decode("UTF-8")
+                sou = BeautifulSoup(a,'html.parser')
+                                
+                if payload in str(respData):
+                        print(colored.green('the payload is reflected on this page ... the site is potentially XSS vulnerable'))
+                        rep = input('Do you want to enter another url ?(y or n) ')
+                        if rep == "Y" or rep == "y":
+                                rl = input(colored.green('Please enter an another url ?(https://www.example.com) :'))
+                                mainT(rl)
                         
                         
                         
                 
-        else :
-                print(colored.red('the payload is not reflexted on this page...Not vulnerable'))
+                else :
+                        print(colored.red('the payload is not reflexted on this page...Not vulnerable'))
+                        rep = input('Do you want to enter another url ?(y or n) ')
+                        if rep == "Y" or rep == "y":
+                                rl = input(colored.green('Please enter an another url ?(https://www.example.com) :'))
+                                mainT(rl)
+
+        except :
+                print("An Error occurred")
                           
+  
                         
                 
 
 
 f = Figlet(font='slant')
-print(colored.blue(f.renderText('XBlue')))
+print(colored.yellow(f.renderText('XBlue')))
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--url", help="specify the url",type=str)
 args = parser.parse_args()
